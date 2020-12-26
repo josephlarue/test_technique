5ed742dd290cd41421575cec61517d5d7f97023b
josephlarue <joseph.larue@edhec.com>
2020-12-26 16:43:48 +0100

    révision du travail sur les validations et modèles et création des views de bases après la création du controller des tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,57 @@
 class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+
+    
+    def new
+        @user = current_user
+        @task = Task.new
+        #authorize @task
+    end
+    
+    def create
+        @user = current_user
+        @task = Task.new (task_params)
+        @task.user = @user
+        #authorize @task
+    if @task.save
+        redirect_to @task
+    else
+        render :index
+    end
+    end
+
+    def show
+        @task = Task.find(params[:id])
+        #authorize @task
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        #authorize @task
+        redirect_to tasks_path
+    end
+
+
+    def edit
+        @task = Task.find(params[:id])
+        #authorize @task
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        #authorize @task
+        redirect_to task_path(@task)
+    end
+
+    
+
+    private
+
+    def task_params
+        params.require(:task).permit(:status, :deadline, :priority_order, :title)
+    end
 end
--- db/migrate/20201226133240_create_tasks.rb
+++ db/migrate/20201226133240_create_tasks.rb
@@ -2,9 +2,10 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.references :user, null: false, foreign_key: true
+      t.string :title
       t.boolean :status
-      t.integer :priority_order
-      t.time :deadline
+      t.string :priority_order
+      t.date :deadline
 
       t.timestamps
     end
