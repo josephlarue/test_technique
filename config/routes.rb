56e83ef9cf1b688b65ada22896e55a45d2ce40ac
josephlarue <joseph.larue@edhec.com>
2020-12-26 14:43:18 +0100

    routing de base

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  end
 end
